21ab2ab9aaaca30a204f4ebbde58c5efaeb7485f
Phestone <just1abacha@gmail.com>
2026-04-21 21:45:58 +0300

    comiit

--- app.js
+++ app.js
@@ -250,7 +250,7 @@ async function announceVibe() {
     ];
     const intro = introVariants[Math.floor(Math.random() * introVariants.length)];
 
-    const fullMessage = `${welcome}. ${greeting}. ${intro} it’s ${dayName}, ${dateStr}. The time is ${timeForVoice}. Just so you know, ${cleanStatus}.`;
+    const fullMessage = `${welcome}. ${greeting}... ${intro} it’s ${dayName}, ${dateStr},. The time is ${timeForVoice}.. Just so you know, ${cleanStatus}.`;
 
     // Clean emojis (keep punctuation for natural speech)
     const cleanText = fullMessage.replace(/[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}]/gu, '');
