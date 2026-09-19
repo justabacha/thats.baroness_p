fe90bd5ad201195f4c9ec7cf0119aaa9346918fb
its phestone <just1abacha@gmail.com>
2026-09-19 12:37:24 +0300

    Comment out export default in handler function

--- api/baroness.js
+++ api/baroness.js
@@ -1,5 +1,5 @@
 // api/baroness.js
-export default async function handler(req, res) {
+//export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
